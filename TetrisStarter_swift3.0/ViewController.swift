7af20e2e66373b569d744f83ff2bab393487222f
Ali A. Kooshesh <kooshesh@students-imac-145.local>
2017-10-02 13:19:52 -0700

    Initial Commit

---
+++ TetrisStarter_swift3.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TetrisStarter_swift3.0
+//
+//  Created by Ali A. Kooshesh on 10/2/17.
+//  Copyright © 2017 Ali A. Kooshesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
